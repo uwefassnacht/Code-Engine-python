09f94aed55f23bab474e1315ac49232c82234a85
Uwe Fassnacht <uwefassnacht@gmail.com>
2023-04-18 14:50:16 +0000

    Updating with changes from Henrik

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World from py server</p>"
+  return "<p>Hello World</p>"
 
+# Get the PORT from environment
+port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=os.getenv('PORT', '8080'))
+	app.run(host='0.0.0.0',port=int(port))
+    
